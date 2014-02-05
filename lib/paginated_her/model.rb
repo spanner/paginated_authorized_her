b033214886cb2ac696d7712ab4184d7d39da675d
William Ross <will@spanner.org>
2014-02-05 15:42:19 +0000

    don't assume api prefix

--- lib/paginated_her/model.rb
+++ lib/paginated_her/model.rb
@@ -6,29 +6,22 @@ module PaginatedHer
     included do
       include_root_in_json true
       after_save :decache
-      after_create :decache_lists
+      after_create :decache_index
     end
     
     def decache
       if $cache
         pk = self.attributes[self.class.primary_key]
-        path = self.class.resource_path.gsub(/:id/, pk.to_s)
-        cache_key = "/api/#{path}"
+        cache_key = self.class.resource_path.gsub(/:id/, pk.to_s)
         p "deleting #{cache_key} from #{$cache.inspect}"
         $cache.delete cache_key
       end
     end
 
-    def decache_lists
-      if $cache
-        pk = self.attributes[self.class.primary_key]
-        path = self.class.collection_path
-        cache_key = "/api/#{path}"
-        p "deleting #{cache_key} from #{$cache.inspect}"
-        $cache.delete cache_key
-      end
+    def decache_index
+      self.class.decache_index
     end
-    
+
     module ClassMethods
       # We are overriding the standard collection with a pagination-respecting equivalent.
       # the parsed_data argument is given in a call from our PaginatedParser, which you can
@@ -47,7 +40,16 @@ module PaginatedHer
           collection
         end
       end
-      
+
+      def decache_index
+        if $cache
+          cache_key = collection_path
+          p "deleting #{cache_key} from #{$cache.inspect}"
+          $cache.delete cache_key
+        end
+      end
+    
+
     end
   end
 
