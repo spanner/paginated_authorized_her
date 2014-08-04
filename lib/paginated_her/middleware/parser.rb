16749af9e32ddac951fe71469e3b5a81e029cfe2
William Ross <will@spanner.org>
2014-08-04 21:28:37 +0100

    debug less

--- lib/paginated_her/middleware/parser.rb
+++ lib/paginated_her/middleware/parser.rb
@@ -1,12 +1,10 @@
 module PaginatedHer::Middleware
   class Parser < Her::Middleware::FirstLevelParseJSON
     def parse(body)
-      Rails.logger.debug "Parser#parse #{body.inspect}"
       super
     end
 
     def on_complete(env)
-      Rails.logger.debug "Parser#on_complete"
       env[:body] = case env[:status]
       when 204
         parse('{}')
