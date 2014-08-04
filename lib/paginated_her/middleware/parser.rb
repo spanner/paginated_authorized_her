26eedf7366f272ed513c42aa32c7a05e2d54f9df
William Ross <will@spanner.org>
2014-08-04 14:10:11 +0100

    bomb if unauth

--- lib/paginated_her/middleware/parser.rb
+++ lib/paginated_her/middleware/parser.rb
@@ -1,12 +1,22 @@
-# https://gist.github.com/letronje/437b4a72225eb53366c6
-#
 module PaginatedHer::Middleware
   class Parser < Her::Middleware::FirstLevelParseJSON
+    def parse(body)
+      Rails.logger.debug "Parser#parse #{body.inspect}"
+      super
+    end
 
     def on_complete(env)
-      super
+      Rails.logger.debug "Parser#on_complete"
+      env[:body] = case env[:status]
+      when 204
+        parse('{}')
+      when 401, 403
+        raise PaginatedHer::AuthRequired, "API responded with auth request or not allowed"
+      else
+        parse(env[:body])
+      end
       env[:body][:pagination] = parse_json(env[:response_headers]["X-Pagination"]) if env[:response_headers]["X-Pagination"].present?
     end
-    
+
   end
 end
