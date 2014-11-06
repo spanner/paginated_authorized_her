90c732a0a5f4ce213a18a39744276837e6e1061c
William Ross <will@spanner.org>
2014-11-06 10:16:30 +0000

    no cache

--- lib/paginated_her/model.rb
+++ lib/paginated_her/model.rb
@@ -5,23 +5,8 @@ module PaginatedHer
     
     included do
       include_root_in_json true
-      after_save :decache
-      after_create :decache_index
     end
     
-    def decache
-      if $cache
-        pk = self.attributes[self.class.primary_key]
-        cache_key = self.class.resource_path.gsub(/:id/, pk.to_s)
-        p "deleting #{cache_key} from #{$cache.inspect}"
-        $cache.delete cache_key
-      end
-    end
-
-    def decache_index
-      self.class.decache_index
-    end
-
     module ClassMethods
       # We are overriding the standard collection with a pagination-respecting equivalent.
       # the parsed_data argument is given in a call from our PaginatedParser, which you can
@@ -40,15 +25,6 @@ module PaginatedHer
           collection
         end
       end
-
-      def decache_index
-        if $cache
-          cache_key = collection_path
-          p "deleting #{cache_key} from #{$cache.inspect}"
-          $cache.delete cache_key
-        end
-      end
-    
 
     end
   end
