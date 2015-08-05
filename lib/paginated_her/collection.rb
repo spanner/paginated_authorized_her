f9172e120999143b865836955d45af1afe5fb72d
William Ross <will@spanner.org>
2015-08-05 16:08:34 +0100

    collect more ducks

--- lib/paginated_her/collection.rb
+++ lib/paginated_her/collection.rb
@@ -5,6 +5,8 @@ module Her
       metadata[:pagination].presence || {}
     end
 
+    # pagination metadata
+
     def current_page
       _pagination_data[:current_page]
     end
@@ -28,5 +30,46 @@ module Her
     def total_count
       _pagination_data[:total_count]
     end
+
+    # ducks for kaminari
+
+    alias :limit_value :per_page
+
+    # pagination helpers
+
+    def last_page?
+      _pagination_data[:current_page] == _pagination_data[:total_pages]
+    end
+
+    def first_page?
+      _pagination_data[:current_page] == 1
+    end
+
+    def first_item_index
+      (current_page - 1) * per_page + 1
+    end
+
+    def last_item_index
+      if last_page?
+        total_count
+      else
+        first_item_index + per_page - 1
+      end
+    end
+
+    # faceting metadata
+
+    def facets
+      metadata[:facets].presence || {}
+    end
+    
+    def facet(name)
+      if facet = facets[name.to_sym]
+        facet[:terms]
+      else
+        {}
+      end
+    end
+
   end
 end
